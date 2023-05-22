b012b204da535df027d4915fb1b236211964b26e
Chili <chilitechnomancy@gmail.com>
2023-05-21 22:01:02 -0400

    Cleaned up index.js

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-const { Pool } = require('pg');
-const readline = require('readline');
+const { Pool } = require('pg')
+const readline = require('readline')
+const dotenv = require('dotenv')
 
-//Init environment vars 
-require('dotenv').config();
 
-//Psql connection
+dotenv.config()
+
 const pool = new Pool({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
-});
+})
 
-//User input
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-const  runQuery = (query) => {
+const runQuery = (query) => {
   return pool.query(query)
     .then((res) => {
-      console.log('Result:');
-      console.table(res.rows);
-      prompt();
+      console.log('Result:')
+      console.table(res.rows)
+      prompt()
     })
     .catch((err) => {
-      console.error(`Error: ${err.message}`);
-      prompt();
-    });
+      console.error(`Error: ${err.message}`)
+      prompt()
+    })
 }
 
 const prompt = () => {
   rl.question('postgres-shell> ', (query) => {
     if (query === '\\q' || query === 'exit') {
-      rl.close();
-      pool.end();
+      rl.close()
+      pool.end()
     }
     else {
-      runQuery(query);
+      runQuery(query)
     }
-  });
+  })
 }
 
-console.log('Welcome to the PostgreSQL shell. Type your SQL queries or "\\q" to exit.');
-prompt();
+console.log('Welcome to the PostgreSQL shell. Type your SQL queries or "\\q" to exit.')
+prompt()
